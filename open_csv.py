998c389d94011a821cab2721e9069b1067e894cb
prashil107 <prashilsingh107@gmail.com>
2025-09-07 23:43:13 +0530

    Generator for output

--- open_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Replace this path with the actual path to your CSV file
-file_path = "emails/emails.csv"
-
-df = pd.read_csv(file_path)
-print("First 5 rows of dataset:")
-print(df.head())
